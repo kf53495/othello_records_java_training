1c071cb00cdc18eb3648fac56976fbb51b888c3f
kf53495 <72958184+kf53495@users.noreply.github.com>
2025-02-26 01:48:35 +0900

    Add files via upload

---
+++ othello_records/Main.java
@@ -0,0 +1,8 @@
+package othello_records;
+
+public class Main {
+    public static void main(String[] args) {
+        String msg = "HelloWorld!";
+        System.out.println(msg);
+    }
+}
---
+++ othello_records/Sample.java
@@ -0,0 +1,8 @@
+package othello_records;
+
+public class Sample {
+    public static void main(String[] args) {
+        String msg = "this is sample!";
+        System.out.println(msg);
+    }
+}
